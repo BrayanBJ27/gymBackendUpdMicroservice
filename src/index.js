0af1177a012a56e7fd46b11362b07f4676279990
BrayanBJ27 <xavi_braian@hotmail.com>
2024-07-07 21:56:04 -0500

    feat: :bug: Fix index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 const app = express();
-const port = process.env.Por || 8090;
+const port = process.env.PORT || 8090;
 
 // Middleware
 app.use(cors());
